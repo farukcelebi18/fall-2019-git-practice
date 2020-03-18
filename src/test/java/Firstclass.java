a0df637dde0c731e9cd443e6f243fcd1061d6b92
farukcelebi18 <farukcelebi1853@gmail.com>
2020-03-18 08:57:13 -0800

    change one

--- src/test/java/Firstclass.java
+++ src/test/java/Firstclass.java
@@ -2,6 +2,7 @@ public class Firstclass {
     public static void main(String[] args) {
         System.out.println("laaooo world");
         System.out.println("someone");
+        System.out.println("change one");
     }
 }
 
