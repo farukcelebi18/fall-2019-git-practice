162ccbb007f1311bcad227b479ba73d142535aee
farukcelebi18 <47309869+farukcelebi18@users.noreply.github.com>
2020-03-18 18:20:28 -0700

    Update Firstclass.java

--- src/test/java/Firstclass.java
+++ src/test/java/Firstclass.java
@@ -2,7 +2,9 @@ public class Firstclass {
     public static void main(String[] args) {
         System.out.println("laaooo world");
         System.out.println("someone");
-         System.out.println("change two");
+        System.out.println("change two");
+        System.out.println("change three");
+
 
     }
 }
